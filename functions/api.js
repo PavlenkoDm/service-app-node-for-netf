c61e62108f5fcf908915b03d00bc540903146925
PavlenkoDm <dmpavlenko07@gmail.com>
2024-04-12 19:07:13 +0300

    change dir for app.js

--- functions/api.js
+++ functions/api.js
@@ -1,28 +1,23 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
-const swaggerUi = require("swagger-ui-express");
-const swaggerDocument = require("./swagger.json");
+const serverLess = require("serverless-http");
 require("dotenv").config();
-require("./crone-jobs/croneJobs");
+require("../crone-jobs/croneJobs");
 
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send({ code: 200, message: "Service app for Netlify is running..." });
+});
 
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
-
-app.use((req, res, next) => {
-  res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
-  res.setHeader("Cross-Origin-Opener-Policy", "same-origin-allow-popups");
-  next();
-});
-
-// app.use("/api/auth", authRouter);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
@@ -33,4 +28,6 @@ app.use((err, req, res, next) => {
   res.status(status).json({ message });
 });
 
-module.exports = app;
+app.use("/.netlify/functions/api", router);
+
+module.exports.handler = serverLess(app);
